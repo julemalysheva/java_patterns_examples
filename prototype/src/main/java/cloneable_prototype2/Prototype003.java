be829632c4bf6e5bc1cd8b1b563e36007479c7a0
julemalysheva <julemalysheva@gmail.com>
2024-05-04 12:01:32 +0500

    Prototype003 Решение Cloneable проблемы

--- prototype/src/main/java/cloneable_prototype2/Prototype003.java
+++ prototype/src/main/java/cloneable_prototype2/Prototype003.java
@@ -13,7 +13,9 @@ public class Prototype003 {
 
         //учитывая что метод вернет Object, нужно явно скастовать к нужному типу.
         //как следствие, тратим время на запаковку и распаковку
-        Worker w2 =(Worker) w1.clone();
+        //Worker w2 =(Worker) w1.clone();
+        //поэтому мы используем констуктор, в который передаем ссылка на объект-прототип
+        Worker w2 = new Worker(w1);
         w2.firstName = "Имя2";
         w2.address[2] = "Просторная";
         /**
@@ -27,10 +29,17 @@ public class Prototype003 {
     }
 }
 
-class Worker implements Cloneable {
+class Worker  { //implements Cloneable (использовали в прошлом примере)
     public String firstName;
     public String lastName;
     public String[] address;
+
+    //наиболее частый прием
+    public Worker(Worker sorce) {
+        this.firstName = sorce.firstName;
+        this.lastName = sorce.lastName;
+        this.address = Arrays.copyOf(sorce.address, sorce.address.length);
+    }
 
     public Worker(String firstName, String lastName, String[] address) {
         this.firstName = firstName;
@@ -46,10 +55,18 @@ class Worker implements Cloneable {
                 Arrays.toString(this.address));
     }
 
+
     //для решения Cloneable проблемы меняем метод, создаем отдельно массив для адреса и делаем его копию
-    @Override
-    public Object clone() throws CloneNotSupportedException {
-        String[] address = Arrays.copyOf(this.address, this.address.length);
-        return new Worker(this.firstName, this.lastName, address);
-    }
+//т.к. мы тратим время чтобы кастовать объекты, используем другой подход.
+//    @Override
+//    public Object clone() throws CloneNotSupportedException {
+//        String[] address = Arrays.copyOf(this.address, this.address.length);
+//        return new Worker(this.firstName, this.lastName, address);
+//    }
 }
+
+/**
+ * И при таком подходе остается проблема, если объект в себе содержит много разных ссылок внутри
+ * Здесь будет другое решение, в сл. примерах. (рефлексия, рекурсивные обходы)
+ * Не уповать на интерфейс Cloneable и помнить, с какими типами работаем.
+ */

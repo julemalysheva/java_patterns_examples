f40a1572d4499817041d9fa3efff891e8a94eb68
julemalysheva <julemalysheva@gmail.com>
2024-05-06 18:04:22 +0500

    a basic example TemplateMethod001

---
+++ template_method/src/main/java/TemplateMethod001.java
@@ -0,0 +1,42 @@
+public class TemplateMethod001 {
+    public static void main(String[] args) {
+        Coffee coffee = new Coffee();
+        coffee.makeElixir();
+
+        Tea tea = new Tea();
+        tea.makeElixir();
+    }
+}
+
+abstract class Elixir {
+    protected void  boidWater() {
+        System.out.println("Вскипятить воду");
+    }
+
+    protected void appendSugar() {
+        System.out.println("Добавить сахар");
+    }
+
+    protected abstract void appendComponent();
+
+    //Шаблонный метод получения напитка
+    public void makeElixir() {
+        boidWater();
+        appendComponent();
+        appendSugar();
+    }
+}
+
+class Tea extends Elixir {
+    @Override
+    protected void appendComponent() {
+        System.out.println("Добавить чай");
+    }
+}
+
+class Coffee extends Elixir {
+    @Override
+    protected void appendComponent() {
+        System.out.println("Добавить кофе");
+    }
+}

21867ae655d1d2637277933e385224e5961d90c4
julemalysheva <julemalysheva@gmail.com>
2024-05-07 15:36:08 +0500

    Proxy003 - Простое логирование через Proxy

--- proxy/src/main/java/v3_logger_proxy/Proxy003.java
+++ proxy/src/main/java/v3_logger_proxy/Proxy003.java
@@ -1,5 +1,55 @@
 package v3_logger_proxy;
-//Простое логирование через Proxy
+//Простое логирование через Proxy.
+// Однако в данном примере логирование нужно явно прописывать в каждом методе
 
 public class Proxy003 {
+    public static void main(String[] args) {
+        Hero hero = new Hero(100, new TerminalLogger());
+        hero.getHp();
+        hero.setMp(111);
+    }
+}
+
+class Hero {
+    private Integer hp;
+    private Integer mp;
+    private Logger logger;
+
+    public Hero(Integer hp, Logger logger) {
+        this.hp = hp;
+        this.mp = 100;
+        this.logger = logger;
+    }
+
+    public Integer getHp() {
+        logger.info("Обращение к getHp");
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        logger.info("Обращение к setHp");
+        this.hp = hp;
+    }
+
+    public Integer getMp() {
+        logger.info("Обращение к getMp");
+        return mp;
+    }
+
+    public void setMp(Integer mp) {
+        logger.info("Обращение к setMp");
+        this.mp = mp;
+    }
+
+}
+
+interface  Logger {
+    void info(String msg);
+}
+
+class TerminalLogger implements  Logger {
+    @Override
+    public void info(String msg) {
+        System.out.println(msg);
+    }
 }
